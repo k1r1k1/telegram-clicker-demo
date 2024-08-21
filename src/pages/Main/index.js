3960105d53fc817a2d02c26f92e3307ad83d5a1c
k1r1k1 <kirillmerkulov@tut.by>
2024-08-21 18:28:37 +0300

    add deployment settings

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../../logo.svg';
 import './App.css';
 
 function MainPage() {
+  const [clicks, setClicks] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <span>Clicks: {clicks}</span>
+        <button className="button-clicker" type="button" onClick={() => setClicks(clicks + 1)}>
+          <img
+            src={logo}
+            className="App-logo"
+            alt="logo"
+          />
+        </button>
         <a
           className="App-link"
           href="https://reactjs.org"
