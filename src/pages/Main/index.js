940430637d0f8c4c5b739a6b142075546bd3d378
k1r1k1 <kirillmerkulov@tut.by>
2024-08-22 21:52:52 +0300

    add donation button

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,30 +1,59 @@
 import React, { useState } from 'react';
-import { TonConnectButton, useTonAddress, useTonWallet } from '@tonconnect/ui-react';
+import {
+  TonConnectButton, useTonAddress, useTonConnectUI, useTonWallet,
+} from '@tonconnect/ui-react';
 import '@twa-dev/sdk';
 
 import logo from '../../logo.svg';
 import './App.css';
+import { AppLogo, DonationButton } from './styled-ui';
 
 function MainPage() {
   const [clicks, setClicks] = useState(0);
+  const [tonConnectUI] = useTonConnectUI();
 
   const userFriendlyAddress = useTonAddress();
+  const userAddress = useTonAddress(false);
   const wallet = useTonWallet();
 
-  console.log({ userFriendlyAddress, wallet });
+  const handleDonate = () => {
+    tonConnectUI.sendTransaction({
+      messages: [
+        {
+          address: '0:390a46d59d88f2a529650d0eafc944f9a9bd21f91d52409051d21898952950b9',
+          amount: '1000000000',
+        },
+      ],
+    })
+      .catch((e) => console.warn(e));
+  };
+
+  console.log({ userFriendlyAddress, userAddress, wallet });
 
   return (
     <div className="App">
       <header className="App-header">
         <div className="head-buttons">
+          {wallet && (
+            <DonationButton
+              type="button"
+              onClick={handleDonate}
+            >
+              Donate 1 TON
+            </DonationButton>
+          )}
           <TonConnectButton />
         </div>
         <span>Clicks: {clicks}</span>
-        <button className="button-clicker" type="button" onClick={() => setClicks(clicks + 1)}>
-          <img
+        <button
+          className="button-clicker"
+          type="button"
+          onClick={() => setClicks(clicks + 1)}
+        >
+          <AppLogo
             src={logo}
-            className="App-logo"
             alt="logo"
+            multiplier={clicks <= 20 ? clicks : 20}
           />
         </button>
         <a
